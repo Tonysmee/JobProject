1b38133ca53b500bd0d81109de34e5812ff29ff4
Tonysmee <tmee8097@gmail.com>
2021-11-27 12:14:58 +0700

    first commit

---
+++ signUpp.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign UP</title>
+</head>
+<body>
+    <h1>Sign up for Job in Laos</h1>
+    
+
+<Label>Username</Label>
+<input type="text" placeholder="username">
+<Label>Password</Label> 
+<input type="password" placeholder="passsword">
+<br>
+<Label>Email</Label> 
+<input type="text" placeholder="Email">
+<Label>Email</Label> 
+<input type="text" placeholder="Phone">
+
+<br>
+<Label>Gender: </Label> 
+  <input name="gender" type="radio" value="F" id="f">
+  <label for="f">Female </label>
+  <input name="gender" type="radio" value="M" id="m">
+  <label for="m">Male</label>
+<br>
+
+<Label>Date of Birth</Label> 
+<input type="date" placeholder="Date of Birth">
+<br>
+
+<Label>Your address</Label>
+<br>
+<textarea name="" placeholder="Address" ></textarea>
+
+
+
+
+
+
+</body>
+</html>
